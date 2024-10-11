b7a25349c798b211cced7627f7cf1960d71b6542
im3dabasia <eshaan.dabasiya@rtcamp.com>
2024-10-11 16:02:58 +0530

    css: Fix button alignment issue and add DOM check

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
 
-const root = ReactDOM.createRoot( document.getElementById( 'root' ) )
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+const rootElement = document.getElementById( 'root' );
+
+if ( rootElement ) {
+    const root = ReactDOM.createRoot( rootElement );
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
